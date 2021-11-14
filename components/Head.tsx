5da5ef4dfd0f88cc454c3afd2ae182bc8f115baa
Rajan Agarwal <rajan.ag005@gmail.com>
2021-11-14 15:19:55 -0500

    custom meta

--- components/Head.tsx
+++ components/Head.tsx
@@ -8,10 +8,9 @@ export const WEBSITE_HOST_URL = 'https://nextjs-typescript-mdx-blog.vercel.app';
 const Head = ({ customMeta }: { customMeta?: MetaProps }): JSX.Element => {
   const router = useRouter();
   const meta: MetaProps = {
-    title: 'Hunter Chang - Website',
+    title: 'Minds On',
     description:
-      'Sleep Deprived Father. Senior Web Developer. Lover of all things Ramen and Kpop.',
-    image: `${WEBSITE_HOST_URL}/images/site-preview.png`,
+      'In our polarized world, stress is omnipresent—and yet finding solutions is more difficult than ever. We are Minds On, a youth nonprofit with a vision.',
     type: 'website',
     ...customMeta,
   };
@@ -27,8 +26,7 @@ const Head = ({ customMeta }: { customMeta?: MetaProps }): JSX.Element => {
       <meta property="og:description" content={meta.description} />
       <meta property="og:title" content={meta.title} />
       <meta property="og:image" content={meta.image} />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@huntarosan" />
+      <meta name="twitter:site" content="@_mindson" />
       <meta name="twitter:title" content={meta.title} />
       <meta name="twitter:description" content={meta.description} />
       <meta name="twitter:image" content={meta.image} />
