4b126c9d2b17679e327b9137d274ace240815690
Rajan Agarwal <64426829+itsrajan@users.noreply.github.com>
2021-12-16 23:40:07 +0000

    new article

--- pages/explore.tsx
+++ pages/explore.tsx
@@ -17,7 +17,7 @@ export const Explore = ({ posts }: IndexProps): JSX.Element => {
       }}
     >
       <h1>Mind Matrix</h1>
-      <p>This is a digital publication for students in Peel that shares out-of-the-box news about pscyhology and the wonders of the mind. In the context of mental health, we write articles about global politics, relationships, technology and wellbeing. Some people say our minds take precedence, others say the world around us is critical: we said, <b>why not both?</b></p>
+      <p>This is a digital publication for students in Peel that shares out-of-the-box news about psychology and the wonders of the mind. In the context of mental health, we write articles about global politics, relationships, technology and wellbeing. Some people say our minds take precedence, others say the world around us is critical: we said, <b>why not both?</b></p>
 			{posts.map((post) => (
         <article key={post.slug} className="mt-12">
           <h1 className="mb-2 text-xl">
